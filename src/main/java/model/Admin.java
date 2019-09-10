bd5cb8c616a9483dd1f5ea99ea80feac7edc2036
Rabeea_Moussa <rabeea96@hotmail.com>
2019-09-10 14:32:06 +0200

    model klasserne

---
+++ src/main/java/model/Admin.java
@@ -0,0 +1,10 @@
+package model;
+
+public class Admin extends User {
+
+    public Admin(int id, String name, String address, int telephoneNumber) {
+        super(id, name, address, telephoneNumber);
+        // TODO Auto-generated constructor stub
+    }
+
+}
