ee862f1a9b6e136e11638e48986cff9c5788591a
Madeleine <madeleine.harbom@gmail.com>
2019-09-13 11:55:30 +0200

    Init klass
    PUP  + route

--- src/main/java/model/PickUpPoint.java
+++ src/main/java/model/PickUpPoint.java
@@ -1,5 +1,17 @@
 package model;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class PickUpPoint {
+	private String afgangsAdresse;
+	private String ankomstAdresse;
+	private double km;
+	private double pris;
+	
+	
+	
+	
+	
 
 }
