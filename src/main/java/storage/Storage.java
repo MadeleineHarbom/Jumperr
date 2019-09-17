a3f4f0dc42913d5fb00c844d7b0074eda60bf4f7
Madeleine <madeleine.harbom@gmail.com>
2019-09-17 10:59:47 +0200

    Storage Trip

--- src/main/java/storage/Storage.java
+++ src/main/java/storage/Storage.java
@@ -2,11 +2,15 @@ package storage;
 
 import java.util.ArrayList;
 
+
 import model.*;
 
 public class Storage {
+	
     public static ArrayList<User> users = new ArrayList<>();
+    public static ArrayList<Trip> trips = new ArrayList<>();
 
+    //users
     public static ArrayList<User> getUsers() {
         return users;
     }
@@ -22,4 +26,22 @@ public class Storage {
             users.remove(user);
         }
     }
+    
+    //trips
+    
+    public static ArrayList<Trip> getTrips() {
+    	return trips;
+    }
+    
+    public static void addTrip(Trip trip) {
+    	if (!trips.contains(trip)) { //saa samme ikke accidently tilfoejes 2 gange
+    		trips.add(trip);
+    	}
+    }
+    
+    public static void removeTrip(Trip trip) {
+    	trips.remove(trip);
+    }
+    
+    
 }
